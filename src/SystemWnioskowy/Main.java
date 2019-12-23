0fe8ed5e5427bddd154276fe07650d74b810fa1b
lukaszk960214 <48412368+lukaszk960214@users.noreply.github.com>
2019-12-23 13:21:08 +0100

    Added Command

--- src/SystemWnioskowy/Main.java
+++ src/SystemWnioskowy/Main.java
@@ -3,6 +3,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+        GroupInsurance groupinsurance = new GroupInsurance();
+		
+		BuyInsurance buyinsurance = new BuyInsurance(groupinsurance);
+		ResignationGroupInsurance resignationinsurance = new ResignationGroupInsurance(groupinsurance);
+		
+		DetailInsurance detailinsurance = new DetailInsurance();
+		detailinsurance.takeDetail(buyinsurance);
+		detailinsurance.takeDetail(resignationinsurance);
+		
+		detailinsurance.placeDetail();
 		Application model=retriveApplicationDataFromDatabase();
 		ApplicationView view= new ApplicationView();
 		ApplicationController controller= new ApplicationController(model,view);
